e1e9f71f126a3c5d149b4c89bd3470d64b6f34c6
Ryland Bell <ryland@Rylands-MacBook-Air.local>
2016-04-22 20:32:50 -0700

    trying to make google fonts work

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,29 +1,29 @@
 /* GET list of patients */
 module.exports.patientList = function(req, res, next) {
-  res.render('patient-list', { title: 'List of Patients' });
+  res.render('patient-list', { title: 'Wasatch: List of Patients' });
 };
 
 /* GET new patient form */
 module.exports.addPatient = function(req, res, next) {
-  res.render('add-patient', { title: 'Add Patient' });
+  res.render('add-patient', { title: 'Wasatch: Add Patient' });
 };
 
 /* GET patient check-in history */
 module.exports.checkinHistory = function(req, res, next) {
-  res.render('check-in-history', { title: 'Check-in History' });
+  res.render('check-in-history', { title: 'Wasatch: Check-in History' });
 };
 
 /* GET update clinician's settings */
 module.exports.clinicianSettings = function(req, res, next) {
-  res.render('clinician-settings', { title: 'My Settings' });
+  res.render('clinician-settings', { title: 'Wasatch: My Settings' });
 };
 
 /* GET edit patient form */
 module.exports.editPatient = function(req, res, next) {
-  res.render('edit-patient', { title: 'Edit Patient' });
+  res.render('edit-patient', { title: 'Wasatch: Edit Patient' });
 };
 
 /* GET edit patient form */
 module.exports.patientInfo = function(req, res, next) {
-  res.render('patient-info', { title: 'Patient Info' });
+  res.render('patient-info', { title: 'Wasatch: Patient Info' });
 };
