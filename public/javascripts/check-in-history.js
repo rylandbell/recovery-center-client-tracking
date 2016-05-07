a5132d830cf8c0a6b7af61b7c6c5a0cff2a5f682
Ryland Bell <ryland@Rylands-MacBook-Air.local>
2016-05-06 19:42:06 -0700

    adds google chart for daily check-in

--- public/javascripts/check-in-history.js
+++ public/javascripts/check-in-history.js
@@ -1,5 +1,6 @@
 // next steps:
 // 'return to today' button
+// fix bug for displaying full month names
 
 
 $(document).ready(function(){
@@ -52,3 +53,79 @@ $(document).ready(function(){
 	}
 })
 
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(function(){
+	drawLineGraph(fudgeData());
+});
+
+
+function fudgeData(){
+	var outputArray = [];
+	var dateArray = [];
+	var today = new Date(2016, 4, 6);
+	for (var i = 0; i<7; i++){
+		var x;
+		x = today.setDate(today.getDate()-1);
+		dateArray.push(x);
+	}
+	for (var i = 0; i<7; i++){
+		var x = [new Date(dateArray[i]),Math.min(i+6,10),10,7-i,Math.floor(Math.random()*10+1),Math.floor(Math.random()*10+1),Math.floor(Math.random()*10+1)];
+		outputArray.push(x);
+	}
+	return outputArray;
+}
+
+function drawLineGraph(dataArray) {
+	var data = new google.visualization.DataTable();
+	data.addColumn('date', 'X');
+	data.addColumn('number', 'Cravings');
+	data.addColumn('number', 'Sleep');
+	data.addColumn('number', 'Stress');
+	data.addColumn('number', 'Mood');
+	data.addColumn('number', 'Energy');
+	data.addColumn('number', 'Goals');
+
+	data.addRows(dataArray);
+
+	var options = {
+		hAxis: {
+		  format: 'EEE, MMM d'
+		  // gridlines: {count:15}
+		},
+		vAxis: {
+		  gridlines: {count: 6},
+		  viewWindowMode: 'pretty'
+		},
+		// title: 'Daily Check-In Results',
+		titleTextStyle: {
+			fontName: 'Droid Serif',
+			fontSize: 24
+		},
+		legend: {
+			position: 'right',
+			alignment: 'center'
+		},
+		height: 500,
+		lineWidth: 3,
+		focusTarget: 'datum',
+		pointSize: 10,
+		// backgroundColor: 'grey',
+		animation: {
+			startup: true,
+			duration: 500,
+			easing: 'out'
+		},
+		aggregationTarget: 'category',
+		chartArea:{
+			height:'80%', 
+			top: 50,
+			left: 50,
+			width: '75%'
+		},
+		fontSize: 18,
+		fontName: 'Droid Sans'
+	};
+
+	var chart = new google.visualization.LineChart(document.getElementById('line-chart'));
+	chart.draw(data,options);
+}
