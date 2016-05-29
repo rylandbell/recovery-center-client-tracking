77f0259e4e715114c339b9a49fa8d0e6ee0e4db7
Ryland Bell <ryland.bell@gmail.com>
2016-05-29 11:16:48 -0700

    adds column charts for boolean check-in data, checks still display incorrectly

--- public/javascripts/check-in-history-columns.js
+++ public/javascripts/check-in-history-columns.js
@@ -1,9 +1,10 @@
 // next steps:
-// lineOptions vs tempOptions is confusing
+// better sidebar nav
+// organize css (multiple files, by page?)
 
 $(document).ready(function(){
 	try {
-		google.charts.load('current', {packages: ['corechart', 'line']});
+		google.charts.load('current', {packages: ['corechart', 'bar']});
 	} catch(e) {
 		$('#chart-error-div').addClass('alert alert-warning').show();
 		$('#chart-error-text').text('Can\'t load the Google Charts package. Are you connected to the internet?');
@@ -12,8 +13,7 @@ $(document).ready(function(){
 
 	var months = ['January', 'February', 'March','April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 	var colorList = [null, '#2C3E50' ,'#18BC9C', '#3498DB', '#F39C12', '#E74C3C', 'darkblue'];
-	var initialLineOptions = {
-		bar: {groupWidth: "95%"},
+	var initialOptions = {
 		colors: colorList,
 		hAxis: {
 		  format: 'EEE, MMM d',
@@ -25,9 +25,10 @@ $(document).ready(function(){
 		  viewWindowMode: 'pretty',
 		  viewWindow: {
 		  	min: 0,
-		  	max: 10
+		  	max: 1
 		  }
 		},
+		bar: {groupWidth: "95%"},
 		titleTextStyle: {
 			fontName: 'Droid Serif',
 			fontSize: 24
@@ -36,8 +37,8 @@ $(document).ready(function(){
 			position: 'top',
 			alignment: 'center',
 		},
-		height: 400,
-		lineWidth: 3,
+		height: 200,
+		lineWidth: 9,
 		focusTarget: 'datum',
 		pointSize: 7,
 		animation: {
@@ -46,75 +47,13 @@ $(document).ready(function(){
 			easing: 'out'
 		},
 		chartArea:{
-			height:'70%', 
+			height:'50%', 
 			top: 50,
 			left: 50,
 			width: '90%'
 		},
 		fontSize: 18,
 		fontName: 'Droid Sans',
-		// Drag-to-pan isn't working as of May 2016, due to a bug on Google's end. Maybe in the future?
-		// explorer: {
-		// 	actions: ['dragToPan'],
-		// 	axis: 'horizontal',
-		// 	keepInBounds: true
-		// }
-	};
-
-	var initialColOptions = {
-		bar: {groupWidth: 70},
-		// colors: colorList,
-		hAxis: {
-		  // format: 'EEE, MMM d',
-		  gridlines: {count: 0},
-		  viewWindow: {}
-		},
-		vAxis: {
-		  gridlines: {count: 0},
-		  // baselineColor: 'white',
-		  viewWindowMode: 'pretty',
-		  viewWindow: {
-		  	min: 0,
-		  	max: 1
-		  }
-		},
-		titleTextStyle: {
-			fontName: 'Droid Serif',
-			fontSize: 24
-		},
-		legend: {
-			position: 'top',
-			alignment: 'center',
-		},
-		height: 100,
-		focusTarget: 'datum',
-		animation: {
-			startup: true,
-			duration: 500,
-			easing: 'out'
-		},
-		chartArea:{
-			height:'70%', 
-			// top: 50,
-			left: 50,
-			width: '90%'
-		},
-		fontSize: 18,
-		fontName: 'Droid Sans',
-		annotations: {
-		    textStyle: {
-		      fontName: 'Times-Roman',
-		      fontSize: 40,
-		      bold: true,
-		      // italic: true,
-		      // The color of the text.
-		      color: '#871b47'
-		      // The color of the text outline.
-		      // auraColor: '#d799ae',
-		      // The transparency of the text.
-		      // opacity: 0.8
-		    }
-		  }
 		// Drag-to-pan isn't working as of May 2016, due to a bug on Google's end. Maybe in the future?
 		// explorer: {
 		// 	actions: ['dragToPan'],
@@ -132,12 +71,15 @@ $(document).ready(function(){
 			x = today.setDate(today.getDate()-1);
 			dateArray.push(x);
 		}
+		// for (var i = 0; i<days; i++){
+		// 	var x = [new Date(dateArray[i]),Math.min(i+6,10),Math.floor(Math.random()*10+1),5,Math.max(7-i,0),Math.floor(Math.random()*10+1),Math.floor(Math.random()*10+1)];
+		// 	outputArray.push(x);
+		// }
 		for (var i = 0; i<days; i++){
-			var med = Math.round(Math.random());
-			var ex = Math.round(Math.random());
-			var x = [new Date(dateArray[i]),Math.min(i+6,10),Math.floor(Math.random()*10+1),5,Math.max(7-i,0),Math.floor(Math.random()*10+1),Math.floor(Math.random()*10+1),Math.round(Math.random()),Math.round(Math.random()),"\u2713"];
+			var x = [new Date(dateArray[i]),Math.round(Math.random())];
 			outputArray.push(x);
 		}
+		console.log(outputArray);
 		return outputArray;
 	}
 
@@ -146,14 +88,6 @@ $(document).ready(function(){
 		var data = new google.visualization.DataTable();
 			data.addColumn('date', 'X');
 			data.addColumn('number', 'Cravings');
-			data.addColumn('number', 'Sleep');
-			data.addColumn('number', 'Stress');
-			data.addColumn('number', 'Mood');
-			data.addColumn('number', 'Energy');
-			data.addColumn('number', 'Goals');
-			data.addColumn('number', 'Meditated');
-			data.addColumn('number', 'Exercised');
-			data.addColumn({type:'string', role:'annotation'});
 			data.addRows(dataArray);
 			return data;
 	}
@@ -174,19 +108,12 @@ $(document).ready(function(){
 		};
 	}
 
-	function initializeCharts(dataArray,lineOptions,colOptions) {
+	function createLineGraph(dataArray,options) {
 		var data = createTable(dataArray);
+		var currentOptions = options;
 		//the Data View is the object that mutates with form input:
-		var filteredLineData = new google.visualization.DataView(data);
-		var exerciseData = new google.visualization.DataView(data);	
-		var meditateData = new google.visualization.DataView(data);	
-
-		exerciseData.setColumns([0,7,9]);
-		meditateData.setColumns([0,8,9]);
-			
-		var lineChart = new google.visualization.LineChart(document.getElementById('line-chart'));
-		var exerciseChart = new google.visualization.ColumnChart(document.getElementById('exercise-chart'));
-		var meditateChart = new google.visualization.ColumnChart(document.getElementById('meditate-chart'));
+		var filteredData = new google.visualization.DataView(data);
+		var chart = new google.visualization.ColumnChart(document.getElementById('line-chart'));
 
 		var viewWidth = 7;
 		var finalDate, earliestDate;
@@ -200,13 +127,13 @@ $(document).ready(function(){
 		function goPast(){
 			earliestDate.setDate(earliestDate.getDate()-viewWidth);
 			finalDate.setDate(finalDate.getDate()-viewWidth);
-			drawCharts(findActiveColumns());
+			drawGraph(findActiveColumns(),currentOptions);
 		}
 
 		function goFuture(){
 			earliestDate.setDate(earliestDate.getDate()+viewWidth);
 			finalDate.setDate(finalDate.getDate()+viewWidth);
-			drawCharts(findActiveColumns());
+			drawGraph(findActiveColumns(),currentOptions);
 		}
 
 		function updateDateDisplay(){
@@ -216,8 +143,8 @@ $(document).ready(function(){
 			$('#date-range').text(rangeString);
 		}
 
-		function drawCharts(cols){
-			filteredLineData.setColumns(cols);
+		function drawGraph(cols,options){
+			filteredData.setColumns(cols);
 
 			$('#go-future').removeClass('disabled');
 			if (finalDate>=dataArray[0][0]){
@@ -225,12 +152,10 @@ $(document).ready(function(){
 				resetDates();
 			}
 			
-			lineOptions.hAxis.viewWindow.max = finalDate;
-			colOptions.hAxis.viewWindow.max = finalDate;
-			lineOptions.hAxis.viewWindow.min = earliestDate;
-			colOptions.hAxis.viewWindow.min = earliestDate;
+			currentOptions.hAxis.viewWindow.max = finalDate;
+			currentOptions.hAxis.viewWindow.min = earliestDate;
 			
-			var tempOptions = lineOptions;
+			var tempOptions = options;
 			var tempColors = [];
 			//start loop at 1 to ignore the null color for x-values (dates)
 			for (var i=1; i<cols.length; i++){
@@ -242,37 +167,31 @@ $(document).ready(function(){
 			// toDataTable method, but I get an error message without it (as far as 
 			// I can tell, this is a bug in the library)
 			updateDateDisplay();
-			lineChart.draw(filteredLineData.toDataTable(),tempOptions);
-
-			// lineOptions.height = 120;
-			// lineOptions.vAxis.viewWindow.max = 1;
-			colOptions.colors=['#2C3E50'];
-			exerciseChart.draw(exerciseData.toDataTable(),colOptions);
-			colOptions.colors=['#18BC9C'];
-			meditateChart.draw(meditateData.toDataTable(),colOptions);
+			chart.draw(filteredData.toDataTable(),tempOptions);
 		}
 
 		function findActiveColumns(){
-			var $selector = $('#toggle-categories');
-			var visibleCols = [0];
-			$selector.children().children().each(function(){
-				if($(this).prop('checked')){
-					visibleCols.push(parseInt($(this).prop('id').substring(3)));
-				}
-			});
+			// var $selector = $('#toggle-categories');
+			// var visibleCols = [0];
+			// $selector.children().children().each(function(){
+			// 	if($(this).prop('checked')){
+			// 		visibleCols.push(parseInt($(this).prop('id').substring(3)));
+			// 	}
+			// });
+			var visibleCols = [0,1];
 			return visibleCols;
 		}
 
 		//redraw when window resized:
 		$(window).on('resize', debounce(
 			function(){
-				drawCharts(findActiveColumns());
+				drawGraph(findActiveColumns(),currentOptions);
 			}
 			,150,false));
 
 		//listen for control panel input:
 		$('#toggle-categories').on('change',function(){
-			drawCharts(findActiveColumns());
+			drawGraph(findActiveColumns(),currentOptions);
 		});
 
 		$('#go-past').on('click',function(){
@@ -305,7 +224,7 @@ $(document).ready(function(){
 			var tempEarliest = new Date(finalDate.getTime());
 			tempEarliest.setDate(finalDate.getDate()-(viewWidth-1));
 			earliestDate = tempEarliest;
-			drawCharts(findActiveColumns());
+			drawGraph(findActiveColumns(),currentOptions);
 		});
 
 		//keyboard controls:
@@ -333,12 +252,13 @@ $(document).ready(function(){
 
 		//initial draw:
 		resetDates();
-		drawCharts(findActiveColumns());
+		drawGraph(findActiveColumns(),currentOptions);
 	}
-
 	
 	google.charts.setOnLoadCallback(function(){
-		initializeCharts(fudgeData(80),initialLineOptions,initialColOptions);
+		createLineGraph(fudgeData(80),initialOptions);
 	});
 });
+
+
 
