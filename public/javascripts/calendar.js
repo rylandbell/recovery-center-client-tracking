93fa3486b7726f56d41b772eebcca6e2dd3137f0
Ryland Bell <ryland.bell@gmail.com>
2016-07-02 10:40:55 -0700

    draws calendar (w/ fake event date) iff authorized

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,3 +1,60 @@
+var fakeEvents = [
+  {
+    title: 'All Day Event',
+    start: '2016-07-01'
+  },
+  {
+    title: 'Long Event',
+    start: '2016-05-07',
+    end: '2016-05-10'
+  },
+  {
+    id: 999,
+    title: 'Repeating Event',
+    start: '2016-05-09T16:00:00'
+  },
+  {
+    id: 999,
+    title: 'Repeating Event',
+    start: '2016-05-16T16:00:00'
+  },
+  {
+    title: 'Conference',
+    start: '2016-05-11',
+    end: '2016-05-13'
+  },
+  {
+    title: 'Meeting',
+    start: '2016-05-12T10:30:00',
+    end: '2016-05-12T12:30:00'
+  },
+  {
+    title: 'Lunch',
+    start: '2016-05-12T12:00:00'
+  },
+  {
+    title: 'Meeting',
+    start: '2016-05-12T14:30:00'
+  },
+  {
+    title: 'Happy Hour',
+    start: '2016-05-12T17:30:00'
+  },
+  {
+    title: 'Dinner',
+    start: '2016-05-12T20:00:00'
+  },
+  {
+    title: 'Birthday Party',
+    start: '2016-05-13T07:00:00'
+  },
+  {
+    title: 'Click for Google',
+    url: 'http://google.com/',
+    start: '2016-05-28'
+  }
+];
+
 $(document).ready(function () {
   var goog = talkToGoogleApi();
 
@@ -87,6 +144,8 @@ $(document).ready(function () {
     goog.listEvents(function (list) {
       console.log(list);
     });
+
+    drawCalendar(fakeEvents);
   }
 
   //check auth on load:
@@ -100,75 +159,23 @@ $(document).ready(function () {
     goog.checkAuth(false, getEvents);
   });
 
-  // //Draw a calendar with fullcalendar.js:
-  // $('#calendar').fullCalendar({
-  //   defaultView: 'agendaWeek',
-  //   header: {
-  //     left: 'prev,next today',
-  //     center: 'title',
-  //     right: 'month,agendaWeek,agendaDay'
-  //   },
-  //   minTime: '07:00',
-  //   maxTime: '21:00',
-  //   // defaultDate: '2016-05-12',
-  //   editable: true,
-  //   eventLimit: true, // allow "more" link when too many events
-  //   events: [
-  //     {
-  //       title: 'All Day Event',
-  //       start: '2016-05-01'
-  //     },
-  //     {
-  //       title: 'Long Event',
-  //       start: '2016-05-07',
-  //       end: '2016-05-10'
-  //     },
-  //     {
-  //       id: 999,
-  //       title: 'Repeating Event',
-  //       start: '2016-05-09T16:00:00'
-  //     },
-  //     {
-  //       id: 999,
-  //       title: 'Repeating Event',
-  //       start: '2016-05-16T16:00:00'
-  //     },
-  //     {
-  //       title: 'Conference',
-  //       start: '2016-05-11',
-  //       end: '2016-05-13'
-  //     },
-  //     {
-  //       title: 'Meeting',
-  //       start: '2016-05-12T10:30:00',
-  //       end: '2016-05-12T12:30:00'
-  //     },
-  //     {
-  //       title: 'Lunch',
-  //       start: '2016-05-12T12:00:00'
-  //     },
-  //     {
-  //       title: 'Meeting',
-  //       start: '2016-05-12T14:30:00'
-  //     },
-  //     {
-  //       title: 'Happy Hour',
-  //       start: '2016-05-12T17:30:00'
-  //     },
-  //     {
-  //       title: 'Dinner',
-  //       start: '2016-05-12T20:00:00'
-  //     },
-  //     {
-  //       title: 'Birthday Party',
-  //       start: '2016-05-13T07:00:00'
-  //     },
-  //     {
-  //       title: 'Click for Google',
-  //       url: 'http://google.com/',
-  //       start: '2016-05-28'
-  //     }
-  //   ]
-  // });
+  //Draw a calendar with fullcalendar.js:
+  function drawCalendar(eventsList) {
+    $('#calendar').fullCalendar({
+      defaultView: 'agendaWeek',
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      minTime: '07:00',
+      maxTime: '21:00',
+
+      // defaultDate: '2016-05-12',
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: eventsList
+    });
+  }
 
 });
